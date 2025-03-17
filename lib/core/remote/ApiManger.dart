528705348e89cd79cc5eedc57518b344980764b0
Ahme43ban <shaabanahmed531@gmail.com>
2025-03-17 14:18:53 +0200

    + solve conflict

--- lib/core/remote/ApiManger.dart
+++ lib/core/remote/ApiManger.dart
@@ -27,4 +27,13 @@ class ApiManger{
     ArticlesResponse articlesResponse=ArticlesResponse.fromJson(json);
     return articlesResponse;
   }
+  Future<ArticlesResponse> searchWay(String text)async{
+    //https://newsapi.org/v2/everything?q=mass&apiKey=7f1cc15c543a4f8cbebb811ef8787e28&pageSize=20
+    Uri url = Uri.https(
+        baseUrl, "v2/everything", {"apikey": apiKey, "q": text});
+    var response = await http.get(url);
+    Map<String,dynamic>json= jsonDecode(response.body);
+    ArticlesResponse articlesResponse = ArticlesResponse.fromJson(json);
+    return articlesResponse;
+  }
 }
